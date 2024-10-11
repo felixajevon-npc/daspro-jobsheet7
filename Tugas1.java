2a3ae647a36b1b1947646ae3709695de4c082919
jevonkagenou <felixalbertajevon@gmail.com>
2024-10-11 09:22:23 +0700

    update java

--- Tugas1.java
+++ Tugas1.java
@@ -7,29 +7,41 @@ public class Tugas1 {
         int jml_tiket;
         int harga_tiket = 50000;
 
+        int total_tiket = 0; 
+        int total_uang = 0; 
+
         while (true) {
-            System.out.print("Masukkan Jumlah Tiket Anda : ");
+            System.out.print("Masukkan Jumlah Tiket Anda (ketik 0 untuk keluar): ");
             jml_tiket = sc.nextInt();
+
+            if (jml_tiket == 0) {
+                System.out.println("Program selesai.");
+                break;
+            }
 
             if (jml_tiket < 0) {
                 System.out.println("Nilai Tidak Valid. Silahkan masukkan lagi nilai yang valid.");
             } else {
-                break;
+                int total_harga = jml_tiket * harga_tiket;
+                total_tiket += jml_tiket;
+                total_uang += total_harga;
+                if (jml_tiket > 10) {
+                    double total_diskon = total_harga * 0.15;
+                    total_harga -= total_diskon;
+                    System.out.println("Anda mendapat diskon 15%");
+                } else if (jml_tiket > 4) {
+                    double total_diskon = total_harga * 0.1;
+                    total_harga -= total_diskon;
+                    System.out.println("Anda mendapat diskon 10%");
+                }
+
+                System.out.println("Total harga tiket: " + total_harga);
             }
         }
 
-        int total_harga = jml_tiket * harga_tiket;
+        System.out.println("Total Tiket yang Dipesan: " + total_tiket);
+        System.out.println("Total Uang yang Diterima: Rp " + total_uang);
 
-        if (jml_tiket > 4) {
-            double total_diskon = total_harga * 0.1;
-            total_harga -= total_diskon;
-            System.out.println("Anda mendapat diskon 10%");
-        } else if (jml_tiket > 10) {
-            double total_diskon = total_harga * 1.5;
-            total_harga -= total_diskon;
-            System.out.println("Anda mendapat diskon 15%");
-        }
-
-        System.out.println("Total harga tiket: " + total_harga);
+        sc.close();
     }
 }
