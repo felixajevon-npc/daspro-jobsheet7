7917681db401fbd7611fc24e4cb1e80447619a0c
jevonkagenou <felixalbertajevon@gmail.com>
2024-10-11 08:57:04 +0700

    update java

--- SiakadFor08.java
+++ SiakadFor08.java
@@ -5,8 +5,10 @@ public class SiakadFor08 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
-
-        for (int i = 1; i <= 10; i++) {
+        int jml_lulus = 0;
+        int jml_tdk_lulus = 0;
+        
+        for (int i = 1; i <= 4; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + " : ");
             nilai = sc.nextDouble();
 
@@ -17,13 +19,17 @@ public class SiakadFor08 {
                 terendah = nilai;
             }
             if (nilai >= 60) {
+                jml_lulus++;
                 System.out.println("Siswa Lulus");
             } else {
+                jml_tdk_lulus++;
                 System.out.println("Siswa Tidak Lulus");
             }
         }
 
         System.out.println("Nilai Tertinggi : " + tertinggi);
         System.out.println("Nilai Terendah : " + terendah);
+        System.out.println("Jumlah Siswa Lulus : " + jml_lulus);
+        System.out.println("Jumlah Siswa Tidak Lulus : " + jml_tdk_lulus);
     }
 }
